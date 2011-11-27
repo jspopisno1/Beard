1a9e1ec67810fb0f08975277648d42cea64529d7
Liangliang Zheng <jspop_no1@hotmail.com>
2011-11-27 15:06:04 +1100

    demo site for beard.js

---
+++ demo/js/action_empty.js
@@ -0,0 +1,8 @@
+Addrbook.actions.empty = function(){
+    $(function(){
+        Addrbook.$body
+        .find('#content .inner')
+        .html('In order to make the max fun of this demo app, you need to \n\
+run it with a browser that supports localStorage.');
+    })
+}
---
+++ demo/js/action_list.js
@@ -0,0 +1,44 @@
+Addrbook.actions.list = function(){
+    $(function(){
+        var $body = Addrbook.$body
+        , $content = $body.find('#content .inner');
+        //    debugger;
+
+        Beard
+        .remote('list.html')
+        .ready(function(){
+            var tmpl = Btpls.Content.List;
+
+            tmpl.extend('Setup', function(item, $t){
+                // to setup each list item
+                // this is to demonstrate how callback works
+
+                $t.initEvents({
+                    'When the mouse hovers on an item //@mouseenter:bind' : function(){
+                        $t.addClass('hover');
+                    },
+                    'When the mouse leave an item //@mouseleave:bind' : function(){
+                        $t.removeClass('hover');
+                    },
+                    'When click to read the item //@:bind': function(){
+                        Beard.utils.log(item);
+                        g.location.hash = '#read/' + item.id;
+                        Addrbook.refreshPage();
+                    }
+                })
+
+                Addrbook.centerImg($t.find('img'));
+            })
+
+            $content
+            .html(tmpl(Addrbook.data))
+            .bindData()
+            .initEvents({
+                'creating a new entry //.create': function(){
+                    Addrbook.refreshPage('create');
+                }
+            });
+        });
+
+    })
+}
